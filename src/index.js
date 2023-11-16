859c96f81a5dd0ff789addee9df8e1fceb5e5e48
Isaiah <IaguileraCFE@gmail.com>
2023-11-16 10:56:26 -0800

    sbmission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-// Your code here
+fetch('http://localhost:3000/characters')
+    .then(response => response.json())
+    .then(data => {
+        const characterBar = document.getElementById('character-bar');
+        data.forEach(character => {
+            const span = document.createElement('span');
+            span.innerText = character.name
+            characterBar.appendChild(span);
+        });
+    });
+
+
+
+
+
+
+
+
+
+
